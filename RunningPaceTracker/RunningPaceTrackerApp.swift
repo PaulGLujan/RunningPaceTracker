e227f640b71fb9348d23f919cfc44dca76cb33ec
Paul <paulglujan@gmail.com>
2025-06-07 05:28:44 -0700

    Initial Commit

---
+++ RunningPaceTracker/RunningPaceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningPaceTrackerApp.swift
+//  RunningPaceTracker
+//
+//  Created by Paul Lujan on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RunningPaceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
